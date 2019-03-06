49d3ad7afd96838062bad3d31a61b9850b1c475b
MukolaKUN <4ydak.mr@gmail.com>
2019-03-06 13:42:26 +0200

    LB_1

---
+++ ConsoleApplication2/ConsoleApplication2/IAccount.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace ConsoleApplication2
+{
+    interface IAccount
+    {
+        int Capital { get; }
+        void SetCapital(int money);
+    }
+}
---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class Account : IAccount
+    {
+        public int Capital { get; protected set; }
+        public virtual void SetCapital(int money)
+        {
+            if (money < 0)
+                throw new Exception("Не можна покласти на рахунок менше 0");
+            this.Capital = money;
+        }
+    }
+    class MicroAccount : IAccount
+    {
+        public int Capital { get; protected set; }
+        public void SetCapital(int money)
+        {
+            if (money < 0)
+                throw new Exception("Не можна покласти на рахунок менше 0");
+            if (money > 100)
+                throw new Exception("Не можна покласти на рахунок більше 100");
+            this.Capital = money;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IAccount acc = new MicroAccount();
+            InitializeAccount(acc);
+            Console.Read();
+        }
+        public static void InitializeAccount(IAccount account)
+        {
+            //account.SetCapital(20); // умова, що задовільняє об’єкти обох класів
+             account.SetCapital(200); // умова, що не задовільняє об’єкт класу MicroAccount
+            Console.WriteLine(account.Capital);
+        }
+    }
+}
